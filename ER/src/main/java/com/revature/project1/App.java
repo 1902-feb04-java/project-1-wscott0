7fd8b6bd5b2d31502b2e6caf6339243f80776459
Will Scott <williamtscott0@gmail.com>
2019-02-22 12:51:14 -0600

    ER/

--- ER/src/main/java/com/revature/project1/App.java
+++ ER/src/main/java/com/revature/project1/App.java
@@ -1,5 +1,10 @@
 package com.revature.project1;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
@@ -8,6 +13,36 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello! regulars" );
+
+        try( 
+            InputStreamReader isr = new InputStreamReader(System.in);
+            BufferedReader br = new BufferedReader(isr);
+        )
+        {
+            System.out.println("please input your employee information: email, username, password( space between each ) ");
+            String input = br.readLine();
+            Scanner scan = new Scanner(input);
+
+            String email = scan.next();
+            String username= scan.next();
+            String password =  scan.next();
+
+            System.out.println(email);
+            System.out.println(username);
+            System.out.println(password);
+
+            
+
+
+            
+        
+        }
+
+        catch (Exception e) {
+            //TODO: handle exception
+            e.printStackTrace();
+        }
+       
+        }
     }
-}
+
