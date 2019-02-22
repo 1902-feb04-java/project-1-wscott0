c0445ed720d9a17b431d59eb2ef2dc300b695f25
Will Scott <williamtscott0@gmail.com>
2019-02-22 10:12:16 -0600

    project1tables.sql

--- project1tables.sql
+++ project1tables.sql
@@ -12,18 +12,30 @@ CREATE TABLE ERS.employee (
 CREATE TABLE ERS.manager (
 	id SERIAL NOT NULL,
 	password CHARACTER VARYING(100) NOT NULL,  -- abbrev VARCHAR
-	login CHARACTER VARYING(100) NOT NULL,-- 4 digits total, 2 after the decimal point
+	login CHARACTER VARYING(100) NOT NULL UNIQUE,-- 4 digits total, 2 after the decimal point
 	title CHARACTER VARYING(100) NOT NULL,
-	email CHARACTER VARYING(100) NOT NULL,
+	email CHARACTER VARYING(100) NOT NULL UNIQUE,
 	firstName CHARACTER VARYING(100) NOT NULL,
 	LastName CHARACTER VARYING(100) NOT NULL
 	
 );
 
 CREATE TABLE ERS.reinbursmentRequest (
-	id SERIAL NOT NULL,
+	reinburse_id SERIAL NOT NULL,
+	reinburse_amount NUMBER NOT NULL,
+	reinburse_Request TIMESTAMP,
+	reinburse_Complete TIMESTAMP,
 	Id_employee_requester int  NOT NULL,
 	requestStatus CHARACTER VARYING(100) NOT NULL
+	
+	
+);
+CREATE TABLE ERS.status  (
+	manager_Login_id  CHARACTER VARYING(100) NOT NULL UNIQUE,
+	manager_email CHARACTER VARYING(100) NOT NULL UNIQUE,
+	reinburse_status_id NUMBER NOT NULL,
+	reinburse_status Character varying(100)
+
 	
 	
 );
